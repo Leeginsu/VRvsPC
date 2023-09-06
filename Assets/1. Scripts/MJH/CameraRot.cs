c89475f20cae106fdd7690cda69ba7945bd99d0a
mjho0225 <mjho0225@gamil.com>
2023-09-06 20:50:56 +0900

    플레이어 애니메이션

---
+++ Assets/1. Scripts/MJH/CameraRot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRot : MonoBehaviour
+{
+
+    float rx, ry;
+
+
+    [Tooltip("카메라 회전 속도")]
+    [Range(1, 300)] public float rotSpeed = 220;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(20, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float my = Input.GetAxis("Mouse Y");
+        ry += my * rotSpeed * Time.deltaTime;
+
+        // 위아래 각도 제한
+        ry = Mathf.Clamp(ry, -30, -5);
+
+        transform.eulerAngles = new Vector3(-ry, 0, 0);
+    }
+}
--- Assets/1. Scripts/MJH/PcPlayer.cs
+++ Assets/1. Scripts/MJH/PcPlayer.cs
@@ -5,24 +5,94 @@ using UnityEngine;
 public class PcPlayer : MonoBehaviour
 {
 
-    public Animator anim;
+    Animator anim;
+    public GameObject player;
 
     // 플레이어 속도
     [Range(1,10)]
     public float moveSpeed = 5f;
 
-    // 플레이어
+    // 플레이어 점프 가능 횟수
+    [SerializeField]
+    int jumpCount = 0;
+    bool isJump = false;
+    public float jumpPower = 10f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpCount = 1;
+        anim = player.GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
     }
+
+
+    void PlayerMove()
+    {
+        float moveZ = 0f;
+        float moveX = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveZ += 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX -= 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveZ -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX += 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCount > 0)
+            {
+                transform.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+                isJump = true;
+                anim.SetTrigger("Jump");
+                jumpCount--;
+            }
+        }
+
+        Vector3 dir = new Vector3(moveX, 0, moveZ);
+        dir.Normalize();
+        transform.Translate(dir * moveSpeed * Time.deltaTime, Space.World);
+
+        if(moveX != 0 || moveZ != 0)
+        {
+            player.transform.rotation = Quaternion.Lerp(player.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 20f);
+        }
+
+        anim.SetFloat("Horizontal", moveZ);
+        anim.SetFloat("Vertical", moveX);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Untagged")
+        {
+            jumpCount = 1;
+            if (isJump == true)
+            {
+                anim.SetTrigger("Land");
+                isJump = false;
+            }
+
+        }
+    }
+
 }
