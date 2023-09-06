e0f1cb8efe154c40114aac9ccf42bcb719623739
devCoderian <87194565+devCoderian@users.noreply.github.com>
2023-09-06 17:08:19 +0900

    photonServersetting 수정

---
+++ Assets/1. Scripts/IA/ConnectionManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("서버 접속");
+        //서버 접속
+        //서버에 App ID, 지역 요청
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    //네임서버 연결 성공(Lobby 진입 불가능)
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        print("서버 연결 접속");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        print("마스터 접속");
+
+
+        //닉네임 설정
+        PhotonNetwork.NickName = "Ian";
+
+        //로비 진입 요청
+        PhotonNetwork.JoinLobby();
+    }
+
+    //로비 진입 성공 시 호출
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        print("씬이동");
+        //PhotonNetwork.LoadLevel("LobbyScene");
+    }
+
+
+}
