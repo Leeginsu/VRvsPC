ec9fa12b8b955a6c1db172552bd28230847dc2fa
mjho0225 <mjho0225@gamil.com>
2023-09-06 16:25:43 +0900

    플레이어 height 변경 및 스크립트 추가

---
+++ Assets/1. Scripts/MJH/PcPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PcPlayer : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // 플레이어 속도
+    [Range(1,10)]
+    public float moveSpeed = 5f;
+
+    // 플레이어
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
