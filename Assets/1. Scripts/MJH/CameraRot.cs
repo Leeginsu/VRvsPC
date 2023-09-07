0967898f91a3e063d09de2c6845b3c6fadc435be
mjho0225 <mjho0225@gamil.com>
2023-09-07 19:41:51 +0900

    애니메이션 추가 및 카메라 조정 완료

--- Assets/1. Scripts/MJH/CameraRot.cs
+++ Assets/1. Scripts/MJH/CameraRot.cs
@@ -9,24 +9,36 @@ public class CameraRot : MonoBehaviour
 
 
     [Tooltip("카메라 회전 속도")]
-    [Range(1, 300)] public float rotSpeed = 220;
+    [Range(1, 300)] public float rotSpeed = 10;
 
+
+
+    private void Awake()
+    {
+        transform.eulerAngles = new Vector3(20, 0, 0);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.eulerAngles = new Vector3(20, 0, 0);
+        //transform.eulerAngles = new Vector3(20, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float my = Input.GetAxis("Mouse Y");
+        
         ry += my * rotSpeed * Time.deltaTime;
 
         // 위아래 각도 제한
         ry = Mathf.Clamp(ry, -30, -5);
 
-        transform.eulerAngles = new Vector3(-ry, 0, 0);
+        transform.eulerAngles = new Vector3(-ry, transform.parent.localEulerAngles.y, 0);
+    
     }
+
+
+    
 }
