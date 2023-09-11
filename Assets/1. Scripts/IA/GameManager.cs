8c7ffcb5e4f5a67bdf08bba93eb552db6abdc118
devCoderian <87194565+devCoderian@users.noreply.github.com>
2023-09-11 17:08:05 +0900

    PlayerSpawnScript 생성

--- Assets/1. Scripts/IA/GameManager.cs
+++ Assets/1. Scripts/IA/GameManager.cs
@@ -1,25 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+public class GameManager : MonoBehaviourPunCallbacks
 {
     public static GameManager instance;
-    
+    public Transform[] PCspawnList;
+    public Transform VRspawnPos;
+    bool isGameReady = false;
+
+    public GameObject TimePanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int PCplayerCnt = PhotonNetwork.CurrentRoom.PlayerCount;
+        PhotonNetwork.Instantiate("VRPlayer", VRspawnPos.position, Quaternion.identity);
+        for (int i = 0; i < PCplayerCnt-1; i++)
+        {
+            PhotonNetwork.Instantiate("Player_Proto", PCspawnList[i].position, Quaternion.identity);
+        }
     }
 
+    float currentTime = 0;
+    float gameTime = 120f;
+    bool isEnd;
+    // Update is called once per frame
+    void SetTime()
+    {
+        gameTime -= currentTime;
+        if(gameTime < 0)
+        {
+            isEnd = true; 
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void setSpawnPos()
-    {
-        
+        SetTime();
+        if (isEnd)
+        {
+            print("게임 종료");
+            //점수 가지고 엔딩씬 넘어가기
+            //PhotonNetwork.LoadLevel("EndingScene");
+        }
     }
 }
