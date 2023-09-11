42462550b8b1cb5d335e24798f2e661a8c1c2f38
devCoderian <87194565+devCoderian@users.noreply.github.com>
2023-09-11 16:37:43 +0900

    GameManagerStash

--- Assets/1. Scripts/IA/GameManager.cs
+++ Assets/1. Scripts/IA/GameManager.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+public class GameManager : MonoBehaviourPunCallbacks
 {
     public static GameManager instance;
-    
+    public Transform[] PCPlayerPosList;
+    public Transform VRPlayerPos;
+
+    int PCPlayerCNT = 0;
+    private void Awake()
+    {
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!ConnectionManager.instance.isVR)
+        {
+            PhotonNetwork.Instantiate("Player", PCPlayerPosList[0].position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
