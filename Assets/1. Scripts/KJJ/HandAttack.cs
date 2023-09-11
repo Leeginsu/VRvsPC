b94feeda412f87a973dc5419053eacd7bf55d33d
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-09-11 20:12:20 +0900

    쇼크웨이브

--- Assets/1. Scripts/KJJ/HandAttack.cs
+++ Assets/1. Scripts/KJJ/HandAttack.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class HandAttack : MonoBehaviour
 {
     public Transform hand;
+    public OVRInput.Controller controller;
 
+    public GameObject wave;
+
+    bool isGrab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,29 +19,62 @@ public class HandAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Ray ray = new Ray(hand.position, -hand.right);
-        //RaycastHit hit;
-
-        //bool isHit = Physics.Raycast(ray, out hit);
-
-
-        //if (isHit)
-        //{
-
-        //}
-        //else
-        //{
-
-        //}
+        // 주먹을 쥔 상태
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, controller))
+        {
+            isGrab = true;
+        }
+        // 주먹을 편 상태
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, controller))
+        {
+            isGrab = false;
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    bool shock = true;
+    // 물리충돌계산
+    private void FixedUpdate()
     {
-        print("무언가 닿았다");
+        if (isGrab)
+        {
+            Collider[] cols = Physics.OverlapSphere(hand.transform.position, 0.5f);
+
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if (cols[i] != null)
+                {
+                    if (cols[i].CompareTag("Ground"))
+                    {
+                        if (shock)
+                        {
+                           
+                           StartCoroutine(ShockWave());
+                            shock = false;
+                        }
+                        //print("쇼크웨이브");
+                    }
+                }
+                print("충돌한 물체 : " + cols[i].gameObject.name);
+            }
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    IEnumerator ShockWave()
     {
-        print("무언가 닿았다");
+        Ray ray = new Ray(hand.position, hand.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                GameObject shockWave = Instantiate(wave);
+                shockWave.transform.localPosition = hitInfo.point + new Vector3(1 * Random.value * 2f, 0, -i);
+                shockWave.transform.forward = hand.transform.forward + new Vector3(0,0,3);
+                shockWave.transform.localScale = new Vector3(1, 1, 1) * Random.Range(1.8f, 2.8f);
+                shockWave.transform.localRotation = Quaternion.Euler(1 * Random.value * 45, 1 * Random.value * 45, 1 * Random.value * 45);
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
     }
 }
