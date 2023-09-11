7bf79204c3b8be62970b09e052054fcfad9b1108
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-09-11 14:59:53 +0900

    .....

--- Assets/Oculus/VR/Scripts/Util/OVRGrabber.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRGrabber.cs
@@ -373,7 +373,7 @@ public class OVRGrabber : MonoBehaviour
             Vector3 angularVelocity =
                 trackingSpace.orientation * OVRInput.GetLocalControllerAngularVelocity(m_controller);
 
-            GrabbableRelease(linearVelocity, angularVelocity);
+            GrabbableRelease(linearVelocity * 30, angularVelocity);
         }
 
         // Re-enable grab volumes to allow overlap events
