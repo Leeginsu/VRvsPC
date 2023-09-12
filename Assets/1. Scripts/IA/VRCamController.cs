5b25bfb10da22c1f3738d21856e3776891b782f0
devCoderian <87194565+devCoderian@users.noreply.github.com>
2023-09-12 14:58:41 +0900

    VR 포톤뷰 캠 활성화

---
+++ Assets/1. Scripts/IA/VRCamController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class VRCamController : MonoBehaviourPun
+{
+    public GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (photonView.IsMine)
+        {
+            cam.SetActive(true);
+        }
+        else
+        {
+            cam.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
