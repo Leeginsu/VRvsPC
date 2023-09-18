43317c110eab4d7642ef380a4b4f776f02a1951d
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-09-18 10:17:50 +0900

    쇼크웨이브 수정중

--- Assets/1. Scripts/KJJ/HandAttack.cs
+++ Assets/1. Scripts/KJJ/HandAttack.cs
@@ -37,17 +37,17 @@ public class HandAttack : MonoBehaviour
     // 물리충돌계산
     private void FixedUpdate()
     {
-        print("isGrab"+isGrab);
+        //print("isGrab"+isGrab);
         if (isGrab)
         {
             //int layerMask = 1 << LayerMask.NameToLayer("Ground");
 
             Collider[] cols = Physics.OverlapSphere(hand.transform.position,0.5f);
 
-            print("cols"+ cols);
+            //print("cols"+ cols);
             for (int i = 0; i < cols.Length; i++)
             {
-                print("colsi" + cols[i]);
+                //print("colsi" + cols[i]);
                 if (cols[i] != null)
                 {
                     if (cols[i].CompareTag("Ground"))
@@ -59,14 +59,13 @@ public class HandAttack : MonoBehaviour
                         }
                     }
                 }
-                print("충돌한 물체 : " + cols[i].gameObject.name);
+               // print("충돌한 물체 : " + cols[i].gameObject.name);
             }
         }
     }
 
     IEnumerator ShockWave()
     {
-        print("6");
         Ray ray = new Ray(hand.position, hand.forward);
         RaycastHit hitInfo;
 
@@ -74,20 +73,14 @@ public class HandAttack : MonoBehaviour
         {
             for (int i = 0; i < 50; i++)
             {
+                print(hitInfo);
                 GameObject shockWave = Instantiate(wave);
-                shockWave.transform.localPosition = hitInfo.point + new Vector3(1 * Random.value * 2f, 0, -i);
+                shockWave.transform.localPosition = hitInfo.point + (hand.forward * i) + new Vector3(1 * Random.value * 2f, 0, 0);
                 //shockWave.transform.forward = hand.transform.forward + new Vector3(0,0,3);
                 shockWave.transform.localScale = new Vector3(1, 1, 1) * Random.Range(1.8f, 2.8f);
                 shockWave.transform.localRotation = Quaternion.Euler(1 * Random.value * 45, 1 * Random.value * 45, 1 * Random.value * 45);
                 Destroy(shockWave.gameObject, 2);
-                //for (int j = 0; j < 10; j++)
-                //{
-                //    GameObject carvingStone = Instantiate(carving);
-                //    carvingStone.transform.localPosition = hitInfo.point + new Vector3(1, 0, -i);
-                //    carvingStone.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f) * Random.Range(0.5f, 2f);
-                //    carvingStone.transform.localRotation = Quaternion.Euler(0, 0, 1 * Random.value * 130);
-                //    Destroy(carvingStone.gameObject, 2);
-                //}
+                
                 yield return new WaitForSeconds(0.01f);
             }
 
