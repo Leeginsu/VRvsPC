e60e514c19f18cddd332c5fb13988fac9e06062f
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-09-21 13:45:15 +0900

    쇼크웨이브 수정하는중

--- Assets/1. Scripts/KJJ/HandAttack.cs
+++ Assets/1. Scripts/KJJ/HandAttack.cs
@@ -13,7 +13,7 @@ public class HandAttack : MonoBehaviourPun
 
     bool isGrab;
 
-    int waveNum = 25;
+    int waveNum = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -90,11 +90,20 @@ public class HandAttack : MonoBehaviourPun
                 //print("ºÓ≈©ø˛¿Ã∫Í!");
                 GameObject shockWave = PhotonNetwork.Instantiate("Wave", new Vector3(0, 0, 0), Quaternion.identity);
                 //GameObject shockWave = Instantiate(wave);
-                Vector3 fx = hitInfo.point + ((dir * (i * 3.5f)) + new Vector3(1 * Random.value * 3f, 0, 0));
+                Vector3 fx = hitInfo.point + ((dir * (i * 4f)) + new Vector3(1 * Random.value * 3f, 0, 0));
                 shockWave.transform.position = new Vector3(0, hitInfo.point.y, 0) + fx;
                 shockWave.transform.localScale = new Vector3(2, 2, 2) * Random.Range(1.0f, 2.0f);
                 shockWave.transform.localRotation = Quaternion.Euler(1 * Random.value * 45, 1 * Random.value * 45, 1 * Random.value * 45);
-                Destroy(shockWave.gameObject, 2);
+                Destroy(shockWave.gameObject, 1f);
+
+                for (int j = 0; j < 2; j++)
+                {
+                    GameObject carving = PhotonNetwork.Instantiate("Carving", new Vector3(0, 0, 0), Quaternion.identity);
+                    carving.transform.position = new Vector3(0, hitInfo.point.y, 0) + fx;
+                    carving.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f) * Random.Range(1.0f, 2.0f);
+                    carving.transform.localRotation = Quaternion.Euler(1 * Random.value * 45, 1 * Random.value * 45, 1 * Random.value * 45);
+                    Destroy(carving.gameObject, 0.7f);
+                }
 
                 yield return new WaitForSeconds(0.05f);
             }
