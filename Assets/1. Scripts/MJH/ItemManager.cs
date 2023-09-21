8bf068dda9d71c5a7514fdc96ed2947003fb3f4b
devCoderian <87194565+devCoderian@users.noreply.github.com>
2023-09-21 16:51:44 +0900

    item manager 개수 생성 수정

--- Assets/1. Scripts/MJH/ItemManager.cs
+++ Assets/1. Scripts/MJH/ItemManager.cs
@@ -10,31 +10,47 @@ public class ItemManager : MonoBehaviourPun
     public GameObject item;
 
     public float delayTime = 0;
+    bool isMaster;
+    private void Awake()
+    {
+        isMaster = PhotonNetwork.IsMasterClient;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        //item = Instantiate(Resources.Load<GameObject>("Item"));
-        item = PhotonNetwork.Instantiate("Item", transform.position, transform.rotation);
-        item.transform.position = transform.position;
-        item.GetComponent<ItemCheck>().mySpawner = this;
-        items.Add(item);
+        if (isMaster)
+        {
+
+            //item = Instantiate(Resources.Load<GameObject>("Item"));
+            item = PhotonNetwork.Instantiate("Item", transform.position, transform.rotation);
+            item.transform.position = transform.position;
+            item.GetComponent<ItemCheck>().mySpawner = this;
+            print(item);
+            items.Add(item);
+            print(items.Count);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (items.Count <= 0)
+        if (isMaster)
         {
-            delayTime += Time.deltaTime;
-            if (delayTime > 3)
+            if (items.Count <= 0)
             {
-                //item = Instantiate(Resources.Load<GameObject>("Item"));
-                item = PhotonNetwork.Instantiate("Item", transform.position, transform.rotation);
-                item.GetComponent<ItemCheck>().mySpawner = this;
-                items.Add(item);
+                print("»ý¼º");
+                delayTime += Time.deltaTime;
+                if (delayTime > 3)
+                {
+                    //item = Instantiate(Resources.Load<GameObject>("Item"));
+                    item = PhotonNetwork.Instantiate("Item", transform.position, transform.rotation);
+                    item.GetComponent<ItemCheck>().mySpawner = this;
+                    items.Add(item);
+                }
             }
         }
+        
     }
     internal void DestroyedItem(ItemCheck item)
     {
